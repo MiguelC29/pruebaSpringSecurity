b848312d65528d0065acc135ff8abac20f6b0467
MiguelC29 <miguelito28c@gmail.com>
2024-07-10 15:54:58 -0500

    changed some deprecated functions on JwtService

--- src/main/java/com/felysoft/felysoftApp/config/JwtService.java
+++ src/main/java/com/felysoft/felysoftApp/config/JwtService.java
@@ -2,11 +2,9 @@ package com.felysoft.felysoftApp.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -41,21 +39,12 @@ public class JwtService {
     ) {
         return Jwts
                 .builder()
-                .setClaims(extraClaims)
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
-                .compact();
-        /*
-        .builder()
                 .claims(extraClaims)
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date(System.currentTimeMillis()))
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey(), Jwts.SIG.HS256)
                 .compact();
-         */
     }
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
@@ -74,26 +63,13 @@ public class JwtService {
     private Claims extractAllClaims(String token) {
         return Jwts
                 .parser()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-        /*
-                return Jwts
-                .parser()
                 .verifyWith(getSignInKey())
                 .build()
                 .parseSignedClaims(token)
-                .getPayload();*/
+                .getPayload();
     }
 
-    /*
     private SecretKey getSignInKey() {
-        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
-        return Keys.hmacShaKeyFor(keyBytes);
-    }*/
-
-    private Key getSignInKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
