f4bdca8d6e90cbaa162d489eda28dc479586829a
MiguelC29 <miguelito28c@gmail.com>
2024-07-12 18:55:51 -0500

    deleted some unused imports

--- src/main/java/com/felysoft/felysoftApp/controller/ProductController.java
+++ src/main/java/com/felysoft/felysoftApp/controller/ProductController.java
@@ -2,19 +2,14 @@ package com.felysoft.felysoftApp.controller;
 
 import com.felysoft.felysoftApp.entity.Product;
 import com.felysoft.felysoftApp.repository.ProductRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/products")
