3680a85729687e9405d008ba2317479c85e6fc53
MiguelC29 <miguelito28c@gmail.com>
2024-07-28 16:10:43 -0500

    add more permissions

--- src/main/java/com/felysoft/felysoftApp/util/Role.java
+++ src/main/java/com/felysoft/felysoftApp/util/Role.java
@@ -15,6 +15,7 @@ public enum Role {
             Permission.CREATE_ONE_PRODUCT,
             Permission.UPDATE_ONE_PRODUCT,
             Permission.DISABLE_ONE_PRODUCT,
+            Permission.READ_INVENTORY_PRODUCTS,
 
             Permission.READ_ALL_CATEGORIES,
             Permission.READ_ONE_CATEGORY,
@@ -71,11 +72,54 @@ public enum Role {
             Permission.UPDATE_ONE_CHARGE,
             Permission.DISABLE_ONE_CHARGE,
 
+            Permission.READ_ALL_EMPLOYEES,
+            Permission.READ_ONE_EMPLOYEE,
+            Permission.CREATE_ONE_EMPLOYEE,
+            Permission.UPDATE_ONE_EMPLOYEE,
+            Permission.DISABLE_ONE_EMPLOYEE,
+
+            Permission.READ_ALL_PAYMENTS,
+            Permission.READ_ONE_PAYMENT,
+            Permission.CREATE_ONE_PAYMENT,
+            Permission.UPDATE_ONE_PAYMENT,
+            Permission.DISABLE_ONE_PAYMENT,
+
+            Permission.READ_ALL_DETAILS,
+            Permission.READ_ONE_DETAIL,
+            Permission.CREATE_ONE_DETAIL,
+            Permission.UPDATE_ONE_DETAIL,
+            Permission.DISABLE_ONE_DETAIL,
+
+            Permission.READ_ALL_EXPENSES,
+            Permission.READ_ONE_EXPENSE,
+            Permission.CREATE_ONE_EXPENSE,
+            Permission.UPDATE_ONE_EXPENSE,
+            Permission.DISABLE_ONE_EXPENSE,
+
+            Permission.READ_ALL_SALES,
+            Permission.READ_ONE_SALE,
+            Permission.CREATE_ONE_SALE,
+            Permission.UPDATE_ONE_SALE,
+            Permission.DISABLE_ONE_SALE,
+
+            Permission.READ_ALL_RESERVES,
+            Permission.READ_ONE_RESERVE,
+            Permission.CREATE_ONE_RESERVE,
+            Permission.UPDATE_ONE_RESERVE,
+            Permission.DISABLE_ONE_RESERVE,
+
             Permission.READ_ALL_USERS,
             Permission.READ_ONE_USER,
             Permission.CREATE_ONE_USER,
             Permission.UPDATE_ONE_USER,
             Permission.DISABLE_ONE_USER,
+
+            Permission.READ_ALL_PURCHASES,
+            Permission.READ_ONE_PURCHASE,
+            Permission.CREATE_ONE_PURCHASE,
+            Permission.UPDATE_ONE_PURCHASE,
+            Permission.DISABLE_ONE_PURCHASE,
+            Permission.READ_EXPENSE_BY_PURCHASE, //
 
             Permission.READ_MY_PROFILE
     )),
